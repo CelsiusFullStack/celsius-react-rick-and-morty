8b391ac641901f1f01a3005c9c61507250f07709
celsiusoft <wilfredo.machado@hotmail.com>
2022-08-17 18:12:29 -0400

    update search ver 0.0.1

--- src/assets/components/LocationInfo.jsx
+++ src/assets/components/LocationInfo.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react"
 const LocationInfo = ({location}) => {
-const [name, setName] = useState()
-const [type, setType] =useState()
-const [dimension, setDimension] = useState()
-const [resident, setResident] = useState()
+const [name, setName] = useState('name')
+const [type, setType] =useState('type')
+const [dimension, setDimension] = useState('dimension')
 useEffect(() =>{
     if(location){ 
         setName(location.name);
         setType(location.type);
         setDimension(location.dimension);
-        
     }
 },[location])
 return(
