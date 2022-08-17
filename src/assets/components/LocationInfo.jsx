9b70031c1f4a5f3594a54962f4dc938419043743
celsiusoft <wilfredo.machado@hotmail.com>
2022-08-17 19:37:32 -0400

    update search ver 0.0.1

--- src/assets/components/LocationInfo.jsx
+++ src/assets/components/LocationInfo.jsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react"
+import getLocation from "../customHooks/getLocation"
 const LocationInfo = ({location}) => {
 const [name, setName] = useState('name')
 const [type, setType] =useState('type')
 const [dimension, setDimension] = useState('dimension')
 useEffect(() =>{
-    if(location){ 
-        setName(location.name);
-        setType(location.type);
-        setDimension(location.dimension);
-    }
-},[location])
+    if(location){
+        getLocation(location).then((locationHead) => {
+        setName(locationHead.name);
+        setType(locationHead.type);
+        setDimension(locationHead.dimension);
+        //alert(name +' '+ type +' '+dimension);
+    })
+    }}, [location])
 return(
-    <div className="title-header-search">
+    <div className="title-location-header">
         <span>{name}</span>
         <span>{type}</span>
         <span>{dimension}</span>
-       
     </div>
 )
 }
