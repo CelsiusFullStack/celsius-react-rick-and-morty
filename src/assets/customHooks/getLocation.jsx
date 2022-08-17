8dbaf14ba8e1642e222772d1216324efab940577
celsiusoft <wilfredo.machado@hotmail.com>
2022-08-17 19:41:01 -0400

    mejorando los estilos en el header location ver 0.0.3

--- src/assets/customHooks/getLocation.jsx
+++ src/assets/customHooks/getLocation.jsx
@@ -3,7 +3,6 @@ const getLocation = (location) => {
       const URL_Location = `https://rickandmortyapi.com/api/location/${location}`;
       const res = axios.get(URL_Location)
       const locationHead = res.then((info) =>info.data);
-
 return locationHead // revisar , par actualizar luego.. se intento pero ya hay q entregar
 }
 export default getLocation
