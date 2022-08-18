164d208000271bb84b20dd2836d064be26b1bf3f
celsiusoft <wilfredo.machado@hotmail.com>
2022-08-17 23:39:11 -0400

    change ver 0.0.5

--- src/assets/components/LocationInfo.jsx
+++ src/assets/components/LocationInfo.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import getLocation from "../customHooks/getLocation"
 const LocationInfo = ({location}) => {
+const [id, setId] = useState('id')
 const [name, setName] = useState('name')
 const [type, setType] =useState('type')
 const [dimension, setDimension] = useState('dimension')
 useEffect(() =>{
     if(location){
         getLocation(location).then((locationHead) => {
+        setId(locationHead.id);
         setName(locationHead.name);
         setType(locationHead.type);
         setDimension(locationHead.dimension);
@@ -15,10 +17,10 @@ useEffect(() =>{
     }}, [location])
 return(
     <div className="title-location-header ">
+        <div className="location-id-circle">{id}</div>
         <span>Name: {name}</span>
         <span>Type: {type}</span>
         <span>Dimension: {dimension}</span>
-
     </div>
 )
 }
